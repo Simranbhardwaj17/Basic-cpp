6111c97ba5299c98980b078cc2c0065a76196bd0
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-07-20 10:30:24 +0530

    File name update

--- namespaceUse.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-  cout << "Hello, CPP Programming \n";
-  cout << "Begin the journey from 19-07-25 \n";
-  cout << "Stay Disciplined, Consistent. DO IT";
-  return 0;
-}
